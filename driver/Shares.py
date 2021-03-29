e96cbbf4a4eb7977ac989bd9f0c703b53f333b68
Cavun Beck <beckcavu@umich.edu>
2021-03-28 23:41:52 -0400

    web progress

--- driver/Shares.py
+++ driver/Shares.py
@@ -4,11 +4,11 @@ import yfinance as yf
 StockShares = ['DOW','YELP','AAPL', 'GME', 'MSFT', 'ELVT', 'ATVI', 'JNJ', 'TSLA']
 
 def main():
-
     for ticker in StockShares:
         openPrice = (yf.Ticker(ticker)).info['open']
         livePrice = si.get_live_price(ticker)
         change = livePrice - openPrice
-        print(ticker + " @ " + str(round(livePrice,2)) + (" up $" if change > 0 else " down $") + str(abs(round(change,2))))
+        print(ticker + " @ " + str(round(livePrice,2)) + (" up $" if change > 0 else " down $") + str(abs(round(change,2))),end='\r')
+
 
 main()
--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-function goToComPage() {
-  window.open("https://dsc.community.dev/university-of-michigan/");
+function goToIL() {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLSeHHlJqfNv7AkHbeaIrZwbhMBZZzfxur6acwmhYQsYKSph1QA/viewform?usp=sf_link");
 }
