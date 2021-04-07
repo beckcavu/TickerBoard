447ea5b8e6383c96380d826a4e3547d91d00d0eb
Cavun Beck <beckcavu@umich.edu>
2021-04-07 13:59:11 -0400

    new format

--- driver/Shares.py
+++ driver/Shares.py
@@ -3,12 +3,15 @@ import yfinance as yf
 
 StockShares = ['DOW','YELP','AAPL', 'GME', 'MSFT', 'ELVT', 'ATVI', 'JNJ', 'TSLA']
 
+
+
+
 def main():
     for ticker in StockShares:
         openPrice = (yf.Ticker(ticker)).info['open']
         livePrice = si.get_live_price(ticker)
         change = livePrice - openPrice
-        print(ticker + " @ " + str(round(livePrice,2)) + (" up $" if change > 0 else " down $") + str(abs(round(change,2))),end='\r')
+        print(ticker + " @ " + str(round(livePrice,2)) + (" up $" if change > 0 else " down $") + str(abs(round(change,2)))+'*'+'\n')
 
 
 main()
